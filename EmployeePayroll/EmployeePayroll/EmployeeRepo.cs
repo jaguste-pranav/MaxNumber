17b2ae9f7b0f27d2bb02eeed59c5c86f81524a10
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-11 04:07:59 +0530

    [pranav] Refactor. Add switch case

---
+++ EmployeePayroll/EmployeePayroll/EmployeeRepo.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (this.connection)
+                {
+                    string query = @"Select * from employee_payroll;";
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    this.connection.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeFirstName = dr.GetString(1);
+                            employeeModel.BasicPay = dr.GetDecimal(2);
+                            employeeModel.StartDate = dr.GetDateTime(3);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(4));
+                            employeeModel.PhoneNumber = dr.GetString(5);
+                            employeeModel.Address = dr.GetString(7);
+                            employeeModel.Department = dr.GetString(6);
+                            employeeModel.Deductions = dr.GetDecimal(8);
+                            employeeModel.TaxablePay = dr.GetDecimal(9);
+                            employeeModel.Tax = dr.GetDecimal(10);
+                            employeeModel.NetPay = dr.GetDecimal(11);
+
+                            Console.WriteLine(employeeModel.EmployeeID + " " + employeeModel.EmployeeFirstName + " " + employeeModel.BasicPay + " " + employeeModel.StartDate + " " + employeeModel.Gender + " " + employeeModel.PhoneNumber + " " + employeeModel.Address + " " + employeeModel.Department + " " + employeeModel.Deductions + " " + employeeModel.TaxablePay + " " + employeeModel.Tax + " " + employeeModel.NetPay);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No data found");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool UpdateSalary(string name, decimal salary)
+        {
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"Update employee_payroll set basic_pay =" + salary + " where name ='" + name + "';";
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+                    this.connection.Open();
+
+                    var result = cmd.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+        public bool AddEmployee(EmployeeModel model)
+        {
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeFirstName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", model.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+        public void GetEmployeesGivenDateRange(DateTime date1, DateTime date2)
+        {
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                SqlCommand command = new SqlCommand("SpGetEmployeesByStartDateRange", this.connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@StartDate1", date1);
+                command.Parameters.AddWithValue("@StartDate2", date2);
+                this.connection.Open();
+
+                SqlDataReader dr = command.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        employeeModel.EmployeeID = dr.GetInt32(0);
+                        employeeModel.EmployeeFirstName = dr.GetString(1);
+                        employeeModel.BasicPay = dr.GetDecimal(2);
+                        employeeModel.StartDate = dr.GetDateTime(3);
+                        employeeModel.Gender = Convert.ToChar(dr.GetString(4));
+                        employeeModel.PhoneNumber = dr.GetString(5);
+                        employeeModel.Address = dr.GetString(7);
+                        employeeModel.Department = dr.GetString(6);
+                        employeeModel.Deductions = dr.GetDecimal(8);
+                        employeeModel.TaxablePay = dr.GetDecimal(9);
+                        employeeModel.Tax = dr.GetDecimal(10);
+                        employeeModel.NetPay = dr.GetDecimal(11);
+
+                        Console.WriteLine(employeeModel.EmployeeID + " " + employeeModel.EmployeeFirstName + " " + employeeModel.BasicPay + " " + employeeModel.StartDate + " " + employeeModel.Gender + " " + employeeModel.PhoneNumber + " " + employeeModel.Address + " " + employeeModel.Department + " " + employeeModel.Deductions + " " + employeeModel.TaxablePay + " " + employeeModel.Tax + " " + employeeModel.NetPay);
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No such records found");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+
+}
---
+++ EmployeePayroll/EmployeePayroll/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Globalization;
+
+namespace EmployeePayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+
+            bool cont = true;
+
+            while (cont)
+            {
+                Console.WriteLine("Choose \n1. View all records \n2. Add record \n3. Update salary \n4. Get employees joined within a date range");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        employeeRepo.GetAllEmployee();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Name ,Department, Phone no, Address, Gender, BasicPay, Deduction, Taxable Pay, Tax, NetPay ");
+                        
+                        Console.WriteLine("Enter Name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter department");
+                        string department = Console.ReadLine();
+
+                        Console.WriteLine("Enter phoneNo");
+                        string phoneNo = Console.ReadLine();
+
+                        Console.WriteLine("Enter address");
+                        string address = Console.ReadLine();
+
+                        Console.WriteLine("Enter gender");
+                        char gender = Convert.ToChar(Console.ReadLine());
+
+                        Console.WriteLine("Enter basic pay");
+                        double basicPay = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter deductions");
+                        double deductions = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter taxable pay");
+                        double taxablePay = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter tax");
+                        double tax = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter netpay");
+                        double netPay = Convert.ToDouble(Console.ReadLine());
+
+                        EmployeeModel employee = new EmployeeModel();
+                        employee.EmployeeFirstName = name;
+                        employee.Department = department;
+                        employee.PhoneNumber = phoneNo;
+                        employee.Address = address;
+                        employee.Gender = gender;
+                        employee.StartDate = DateTime.Today;
+                        employee.BasicPay = Convert.ToDecimal(basicPay);
+                        employee.Deductions = Convert.ToDecimal(deductions);
+                        employee.TaxablePay = Convert.ToDecimal(taxablePay);
+                        employee.Tax = Convert.ToDecimal(tax);
+                        employee.NetPay = Convert.ToDecimal(netPay);
+
+                        employeeRepo.AddEmployee(employee);
+
+                        Console.WriteLine("Records added successfully");
+                        break;
+                    case 3:
+                        employeeRepo.UpdateSalary("Bill", 150000);
+                        Console.WriteLine("Salary updated");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter Dates");
+                        string[] dates = Console.ReadLine().Split(",");
+
+                        employeeRepo.GetEmployeesGivenDateRange(Convert.ToDateTime(dates[0]), Convert.ToDateTime(dates[1]));
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+            }
+        }
+    }
+}
